0e931eb001d9c7ca8ee939259a1220d9db2154bd
Nihar Mishra <niharmishra11@gmail.com>
2025-03-10 20:04:31 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,24 @@
 import pyodbc
 
 # Define connection parameters
-conn_str = "DRIVER={Adaptive Server Enterprise};SERVER=your_server;PORT=5000;DATABASE=your_db;UID=username;PWD=password"
+conn_str = (
+    "DRIVER={Adaptive Server Enterprise};"
+    "SERVER=your_server;"
+    "PORT=5000;"
+    "DATABASE=your_db;"
+    "UID=your_username;"
+    "PWD=your_password"
+)
 
 # Establish connection
 conn = pyodbc.connect(conn_str)
 cursor = conn.cursor()
 
 # Execute a query
-cursor.execute("SELECT * FROM your_table")
+query = "SELECT * FROM your_table"
+cursor.execute(query)
+
+# Fetch results
 rows = cursor.fetchall()
 
 # Print results
